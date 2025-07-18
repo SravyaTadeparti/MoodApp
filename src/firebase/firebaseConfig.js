51f6a9d183005fb9ea2fa076bfab6c0c91e5cf14
whynimisha <your-email@example.com>
2025-07-18 12:11:22 +0530

    Add firebaseConfig.js

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyC7Zi_YwwT5GaqL95jIb0RslHlu0pTJJiQ",
+  authDomain: "mood-checkin-ef2f4.firebaseapp.com",
+  projectId: "mood-checkin-ef2f4",
+  storageBucket: "mood-checkin-ef2f4.firebasestorage.app",
+  messagingSenderId: "1015907097608",
+  appId: "1:1015907097608:web:0eb5cb6ea2db5df26785bd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
